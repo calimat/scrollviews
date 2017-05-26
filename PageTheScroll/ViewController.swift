f43e415f6fd07bee8cd8b8fea723a968b1074cf0
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2017-05-26 12:11:27 -0500

    finished up with scrollview time for excercises

--- PageTheScroll/ViewController.swift
+++ PageTheScroll/ViewController.swift
@@ -21,7 +21,15 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         
+           }
+
+    override func viewDidAppear(_ animated: Bool) {
+        
+        print("Scrollview width:\(scrollView.frame.size.width)")
+
+        
         var contentWidth:CGFloat = 0
+        let scrollWidth = scrollView.frame.size.width
         
         for x in 0...2 {
             let image = UIImage(named: "icon\(x).png")
@@ -29,18 +37,20 @@ class ViewController: UIViewController {
             images.append(imageView)
             
             var newX:CGFloat = 0.0
-            newX = view.frame.midX + view.frame.size.width * CGFloat(x)
+            newX = scrollWidth / 2  + scrollWidth * CGFloat(x)
             
             contentWidth += newX
             
             scrollView.addSubview(imageView)
             
-            imageView.frame = CGRect(x: newX - 75, y: (view.frame.size.height / 2) - 75, width: 150, height: 150)
+            imageView.frame = CGRect(x: newX - 75, y: (scrollView.frame.size.height / 2) - 75, width: 150, height: 150)
         }
         
-       scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
-    }
+        
+        scrollView.clipsToBounds = false
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
 
+    }
    
 
 }
